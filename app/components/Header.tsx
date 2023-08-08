d780db4ffc547b78e6c58758f10d3809fb5384e9
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-08-09 03:26:57 +0700

    first commit

---
+++ app/components/Header.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header = ({ children, className }: HeaderProps) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // TODO: Handle logout later
+  };
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition-opacity"
+          >
+            <RxCaretLeft size={35} className="text-white" />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition-opacity"
+          >
+            <RxCaretRight size={35} className="text-white" />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition-opacity">
+            <HiHome size={20} className="text-black" />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition-opacity">
+            <BiSearch size={20} className="text-black" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
